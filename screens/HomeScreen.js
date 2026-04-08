c61bab1e490e75d7db946cade85a1ebe505eb8b6
jerog1971 <jerog1971@gmail.com>
2026-04-07 23:27:12 -0300

    Add files via upload
    atualizados

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,6 +9,7 @@ const IMAGENS_LOCAIS = {
   'bolo_cenoura.jpg': require('./img/bolo_cenoura.jpg'),
   'bolo_laranja.jpg': require('./img/bolo_laranja.jpg'),
   'pao_de_queijo.jpg': require('./img/pao_de_queijo.jpg'),
+  'bolo_caneca.jpg': require('./img/bolo_caneca.jpg'),
   'mousse_limao.jpg': require('./img/mousse_limao.jpg'),
 };
 
