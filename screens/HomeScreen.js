2b09016787cf85dd327740a2b39cf913bc98344e
jerog1971 <jerog1971@gmail.com>
2026-08-07 18:56:31 -0300

    Fix fetch URL for receitas.json
    Updated the fetch URL for receitas.json to use raw GitHub content.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,7 @@ export default function HomeScreen({ navigation }) {
   const sincronizarReceitas = async () => {
     setCarregando(true);
     try {
-      const response = await fetch('https://github.com/jerog1971/projeto-mobile/refs/heads/main/receitas.json');
+      const response = await fetch('https://raw.githubusercontente.com/jerog1971/projeto-mobile/refs/heads/main/receitas.json');
       const receitasDoServidor = await response.json();
       const novas = receitasDoServidor.filter(resServidor => !receitas.some(resLocal => resLocal.id === resServidor.id));
       
